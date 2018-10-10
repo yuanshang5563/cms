ddf4715d71bcf077993c8de6925389d5fb00a4a1
yuanshang5563 <yuanshang5563@163.com>
2018-10-10 23:44:17 +0800

    SQL

--- cms-mybatis-plus/sql.sql
+++ cms-mybatis-plus/sql.sql
@@ -6,6 +6,20 @@ CREATE TABLE `core_user` (
   `phone` varchar(20) DEFAULT NULL,
   `birthday` date DEFAULT NULL,
   `comment_info` varchar(2000) DEFAULT NULL,
+  `created_time` datetime DEFAULT NULL,
+  `modified_time` datetime DEFAULT NULL,
   PRIMARY KEY (`core_user_id`),
   UNIQUE KEY `pk_core_user_id` (`core_user_id`) USING HASH
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `cms_log` (
+  `cms_log_id` varchar(40) NOT NULL,
+  `log_type` varchar(30) DEFAULT NULL,
+  `created_time` datetime DEFAULT NULL,
+  `modified_time` datetime DEFAULT NULL,
+  `module` varchar(200) DEFAULT NULL,
+  `user` varchar(100) DEFAULT NULL,
+  `ip_address` varchar(40) DEFAULT NULL,
+  `log_info` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`cms_log_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
