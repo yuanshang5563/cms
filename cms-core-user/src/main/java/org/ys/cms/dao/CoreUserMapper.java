21e3d76bced82dad0d8d22cf9cf3666d4bed67fb
yuanshang5563 <yuanshang5563@163.com>
2018-11-24 13:25:16 +0800

    change to cms-

---
+++ cms-core-user/src/main/java/org/ys/cms/dao/CoreUserMapper.java
@@ -0,0 +1,17 @@
+package org.ys.cms.dao;
+
+import org.ys.cms.model.CoreUser;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author YS
+ * @since 2018-10-05
+ */
+public interface CoreUserMapper extends BaseMapper<CoreUser> {
+
+}
